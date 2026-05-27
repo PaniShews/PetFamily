a3ff36436b816308a496c0f783b950cc1093ee31
PaniShews <kpetruh@gmail.com>
2026-05-27 16:51:21 +0300

    Fix admin imports

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from models import User, BreederShelterProfile
+from .models import User, BreederShelterProfile
 
 
 @admin.register(User)
